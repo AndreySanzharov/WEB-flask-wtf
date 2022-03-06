208dcb1d510ff0d24847f043af64ec4951f956eb
AndreySanzharov <83120731+AndreySanzharov@users.noreply.github.com>
2022-03-06 18:27:24 +0300

    Add files via upload

---
+++ Getting ready for the mission/Getting ready for the mission.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import url_for
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {}
+    param['zagolowok'] = "Миссия Колонизация Марса"
+    param['title'] = 'Заготовка'
+    return render_template('base.html', **param)
+
+
+if __name__ == "__main__":
+    app.run(port=8000, host='127.0.0.1')
